de292af1aade129394e307ce7335ece8011af07b
sheritsh <sheritsh@student.21-school.ru>
2023-09-16 08:51:40 +0300

    feat: simple nodejs task

--- src/chapter_2/fsSimple.js
+++ src/chapter_2/fsSimple.js
@@ -1,7 +1,71 @@
 const fs = require("fs");
+const path = require("path");
 
-const readAndWriteCallbackHell = () => {};
+const fileToRead = path.resolve(__dirname, 'files/fsSimple/file1.txt');
+const fileToWrite = path.resolve(__dirname, 'files/fsSimple/file2.txt');
 
-const readAndWritePromises = () => {};
+// Function to read and write data using callback hell
+const readAndWriteCallbackHell = () => {
+  fs.readFile(fileToRead, 'utf8', (err, data) => {
+    if (err) {
+      throw new Error('Error reading file:', err);
+    }
 
-const readAndWriteAsyncAwait = async () => {};
+    fs.writeFile(fileToWrite, data, 'utf8', (err) => {
+      if (err) {
+        throw new Error('Error writing file:', err);
+      }
+
+      console.log('Data successfully copied.');
+    });
+  });
+};
+
+// Function to read and write data using promises
+const readAndWritePromises = () => {
+  const readFilePromise = new Promise((resolve, reject) => {
+    fs.readFile(fileToRead, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+
+  readFilePromise
+    .then((data) => {
+      return new Promise((resolve, reject) => {
+        fs.writeFile(fileToWrite, data, 'utf8', (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve('Data successfully copied.');
+          }
+        });
+      });
+    })
+    .then((message) => {
+      console.log(message);
+    })
+    .catch((err) => {
+      console.error('Error', err);
+    });
+};
+
+// Function to read and write data using async/await
+const readAndWriteAsyncAwait = async () => {
+  try {
+    const data = await fs.promises.readFile(fileToRead, 'utf8');
+    await fs.promises.writeFile(fileToWrite, data, 'utf8');
+    console.log('Data successfully copied.');
+  } catch (err) {
+    console.error('Error:', err);
+  }
+};
+
+/* Usage */
+
+// readAndWriteCallbackHell();
+// readAndWritePromises();
+// readAndWriteAsyncAwait();
